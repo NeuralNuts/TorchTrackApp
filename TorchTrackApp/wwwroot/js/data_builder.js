d2f839360a75daec60d066c32ace6f823c7e5d00
Zakk <Zakk@DESKTOP-195CIHE>
2023-10-11 13:18:23 +1000

    Misc

--- TorchTrackApp/wwwroot/js/data_builder.js
+++ TorchTrackApp/wwwroot/js/data_builder.js
@@ -1,4 +1,4 @@
-temp_array = [];
+let temp_array = [];
 
 function get_torch_track_data(model_name) {
     $.ajax({
@@ -9,7 +9,8 @@ function get_torch_track_data(model_name) {
             try {
                 temp_array = data
                 req_data_parser(temp_array)
-                console.log(temp_array)
+                //loopThrough(temp_array)
+                //console.log(temp_array)
 
             } catch (error) {
                 console.error('Error processing data:', error.message);
@@ -21,14 +22,33 @@ function get_torch_track_data(model_name) {
     });
 }
 
-function req_data_parser(data) {
-    //var table = document.getElementById('lists-select')
-    for (var i = 0; i < data.length; i++) {
-        //var row = `<option>${data[i].listName}</option>`
-        //table.innerHTML += row
-        const d = JSON.parse(data[i].model_optimizer);
-        console.log(d.state);
+function html_builder(key, data) {
+    let parent_data_div = document.getElementById("parent-data-id");
+    var h2 = `<h2>${key}</h2>`;
+
+    parent_data_div.innerHTML += h2;
+}
+
+function loopThrough(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(key, obj[key]);
+            html_builder(key, obj[key])
+
+            if (typeof obj[key] === 'object') {
+                loopThrough(obj[key]);
+            }
+        }
     }
 }
 
-get_torch_track_data("Test");
+function req_data_parser(data) {
+    for (var i = 0; i < data.length; i++) {
+        const parsed_json = JSON.parse(data[i].model_optimizer, null, 4);
+        let html_p = document.getElementById("p-id")
+
+        loopThrough(parsed_json)
+    }
+}
+
+get_torch_track_data("Bing");
