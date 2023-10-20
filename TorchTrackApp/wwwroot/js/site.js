208d6c7354fa998821ae53897395b8a0a52f80de
Zakk <Zakk@DESKTOP-195CIHE>
2023-10-21 02:02:28 +1000

    misc

--- TorchTrackApp/wwwroot/js/site.js
+++ TorchTrackApp/wwwroot/js/site.js
@@ -12,3 +12,18 @@
     }
 });
 
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const href = this.getAttribute('href');
+        const target = document.querySelector(href);
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'  // align to top
+            });
+        }
+    });
+});
